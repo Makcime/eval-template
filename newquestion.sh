e9e89dd6fa25704a43bac743ae3d6d2073774e9c
Makcime <marlier.maxime@gmail.com>
2023-09-30 23:47:43 +0200

    [add] useful scripts

---
+++ newquestion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the highest existing question number
+max_q_num=$(ls src/questions/q*.tex 2>/dev/null | sed 's/src\/questions\/q//;s/\.tex//' | sort -n | tail -1)
+
+# If there are no existing questions, set the starting number to 0
+if [[ -z "$max_q_num" ]]; then
+    max_q_num=0
+fi
+
+# Increment the number
+next_q_num=$((max_q_num + 1))
+
+# Create the new files
+cp  "src/questions/qx.tex" "src/questions/q${next_q_num}.tex"
+echo "\inlineanswer{a${next_q_num}}" >> "src/questions/q${next_q_num}.tex"
+cp  "src/answers/ax.tex" "src/answers/a${next_q_num}.tex"
+
+# Feedback to the user
+echo "Created src/uestions/q${next_q_num}.tex and src/answers/a${next_q_num}.tex"
+
